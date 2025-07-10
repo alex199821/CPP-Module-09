4b51a552745c87a59cee97d37ec063f6a381010c
auplisas <aupli16@freeuni.edu.ge>
2025-07-11 00:03:41 +0200

    change approach to tokenization

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: auplisas <auplisas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 22:07:47 by auplisas          #+#    #+#             */
-/*   Updated: 2025/07/10 23:03:08 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/07/11 00:02:12 by auplisas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 bool	isOperator(char c)
 {
 	return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+int applyOperator(int a, int b, char op) {
+    if (op == '+') 
+        return a + b;
+    if (op == '-') 
+        return a - b;
+    if (op == '*') 
+        return a * b;
+    if (op == '/') {
+        if (b == 0) 
+            throw std::runtime_error("Division by zero");
+        return a / b;
+    }
+    throw std::runtime_error("Invalid operator");
 }
 
 int main(int argc, char **argv)
@@ -26,39 +41,54 @@ int main(int argc, char **argv)
     }
 
     std::string arg = argv[1];
-
-    std::deque<int> digitsAr;
+    std::istringstream iss(arg);
+    std::string token;
+    std::vector<std::string> tokens;
     std::deque<char> operatorsAr;
 
-    for (size_t i = 0; i < arg.size(); i++)
+    while (iss >> token)
     {
-        if (isdigit(arg[i]))
+        if ((token.length() == 1 && isdigit(token[0])) ||
+            (token.length() == 1 && isOperator(token[0])))
         {
-            digitsAr.push_back(arg[i] - '0');
+            tokens.push_back(token);
         }
-        else if (isOperator(arg[i]))
+        else
         {
-            operatorsAr.push_back(arg[i]);
+            std::cerr << "Error" << std::endl;
+            return 1;
         }
     }
+    
+    // for (size_t i = 0; i < arg.size(); i++)
+    // {
+    //     if (isdigit(arg[i]))
+    //     {
+    //         digitsAr.push_back(arg[i] - '0');
+    //     }
+    //     else if (isOperator(arg[i]))
+    //     {
+    //         operatorsAr.push_back(arg[i]);
+    //     }
+    // }
 
-    std::cout << "Operators: ";
-    for (size_t i = 0; i < operatorsAr.size(); ++i)
-    {
-        std::cout << operatorsAr[i] << " ";
-        if (operatorsAr[i] == '*' || operatorsAr[i] == '/')
-        {
-            // handle precedence if needed
-        }
-    }
-    std::cout << std::endl;
+    // std::cout << "Operators: ";
+    // for (size_t i = 0; i < operatorsAr.size(); ++i)
+    // {
+    //     std::cout << operatorsAr[i] << " ";
+    //     if (operatorsAr[i] == '*' || operatorsAr[i] == '/')
+    //     {
+    //         // handle precedence if needed
+    //     }
+    // }
+    // std::cout << std::endl;
 
-    std::cout << "Digits: ";
-    for (size_t i = 0; i < digitsAr.size(); ++i)
-    {
-        std::cout << digitsAr[i] << " ";
-    }
-    std::cout << std::endl;
+    // std::cout << "Digits: ";
+    // for (size_t i = 0; i < digitsAr.size(); ++i)
+    // {
+    //     std::cout << digitsAr[i] << " ";
+    // }
+    // std::cout << std::endl;
 
     return 0;
 }
